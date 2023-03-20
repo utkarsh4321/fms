8d17919e4691b8fbcb36c3b11a750ee5ecdae9cd
utkarsh srivastava <30936607+utkarsh4321@users.noreply.github.com>
2023-03-20 19:24:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('utkarsh are you doing great!Keep going')
 })
+app.get('/blog', (req, res) => {
+  res.send('All the blog written by utkarsh')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
